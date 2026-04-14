1cdbbd95ac4ba05d72bdb79528057c10674b38f3
mattrubi1 <matt@nicandmatt.com>
2026-04-14 17:14:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import streamlit as st
-from db import read_data
+import pandas as pd
 
 st.title("Retail ML System")
 
-df = read_data()
+# LOAD DATA FROM GITHUB RAW FILE
+url = "https://raw.githubusercontent.com/YOUR_USERNAME/retail-ml-system/main/data.csv"
+
+df = pd.read_csv(url)
 
 st.dataframe(df)
 
 st.subheader("Top Items")
 st.dataframe(df.sort_values("ml_score", ascending=False))
+
