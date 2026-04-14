32d616d9e784f20bb5a18f91b49ea9b045f53bd7
mattrubi1 <matt@nicandmatt.com>
2026-04-14 16:53:48 -0400

    Add function to send alerts via Telegram

---
+++ alerts.py
@@ -0,0 +1,8 @@
+import requests
+
+TOKEN = 8264216833:AAHFFfTVju6fY5ogTuenSUDQ5SutUs9DxN4
+CHAT_ID = 7938644912
+
+def send_alert(msg):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    requests.post(url, json={"chat_id": CHAT_ID, "text": msg})
