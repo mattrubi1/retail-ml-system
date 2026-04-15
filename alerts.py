028c32aba417d2ad8f3963aaf17c2bd975270d8a
mattrubi1 <matt@nicandmatt.com>
2026-04-14 21:56:03 -0400

    Refactor send_alert function parameter and formatting

--- alerts.py
+++ alerts.py
@@ -1,8 +1,13 @@
 import requests
 
-TOKEN = "8264216833:AAHFFfTVju6fY5ogTuenSUDQ5SutUs9DxN4"
-CHAT_ID = "7938644912"
+TOKEN = “8264216833:AAHFFfTVju6fY5ogTuenSUDQ5SutUs9DxN4”
+CHAT_ID = “7938644912”
 
-def send_alert(msg):
+def send_alert(message):
+
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    requests.post(url, json={"chat_id": CHAT_ID, "text": msg})
+
+    requests.post(url, json={
+        "chat_id": CHAT_ID,
+        "text": message
+    })
