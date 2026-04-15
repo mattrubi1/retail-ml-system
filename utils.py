e1fbc2f41d4f49956a51c5ef3b61768c14bfe3d6
mattrubi1 <matt@nicandmatt.com>
2026-04-15 15:50:00 -0400

    Refactor SKU generation to use base and hash parts

--- utils.py
+++ utils.py
@@ -3,10 +3,10 @@ import hashlib
 
 def generate_store_sku(item_name: str, store_id: str):
 
-    raw = f"{store_id}-{item_name}"
-    short_hash = hashlib.md5(raw.encode()).hexdigest()[:6].upper()
+    base = f"{store_id}-{item_name}"
+    hash_part = hashlib.md5(base.encode()).hexdigest()[:6].upper()
 
-    return f"{store_id}-{item_name[:5].upper()}-{short_hash}"
+    return f"{store_id}-{item_name[:6].upper()}-{hash_part}"
 
 
 def normalize_sku(sku: str):
