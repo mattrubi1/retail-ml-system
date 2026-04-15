171b3738425bffc80eb47312e31ddfd0ea7b609d
mattrubi1 <matt@nicandmatt.com>
2026-04-15 14:04:53 -0400

    Refactor generate_store_sku function
    Removed docstring from generate_store_sku function and simplified hash generation.

--- utils.py
+++ utils.py
@@ -2,15 +2,9 @@ import hashlib
 
 
 def generate_store_sku(item_name: str, store_id: str):
-    """
-    Creates a stable SKU based on store + item name
-    Same input ALWAYS produces same SKU
-    """
 
     key = f"{store_id}-{item_name}"
-
-    hash_obj = hashlib.md5(key.encode())
-    hash_hex = hash_obj.hexdigest()
+    hash_hex = hashlib.md5(key.encode()).hexdigest()
 
     return f"SKU-{hash_hex[:3]}-{hash_hex[3:6]}-{hash_hex[6:9]}"
 
