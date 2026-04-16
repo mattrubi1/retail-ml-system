b08b97ba6105b7f7d942a0558c3a6049f2018b82
mattrubi1 <matt@nicandmatt.com>
2026-04-16 06:51:51 -0400

    Refactor fetch_products to fetch_all_products

--- data_source.py
+++ data_source.py
@@ -1,19 +1,5 @@
-from category_graph import crawl
+from product_source import fetch_products
 
 
-def fetch_products():
-
-    urls = crawl(max_depth=2)
-
-    products = []
-
-    for u in urls:
-
-        name = u.split("/")[-1].replace("-", " ")
-
-        products.append({
-            "name": name.title(),
-            "url": u.split("?")[0]
-        })
-
-    return products
+def fetch_all_products():
+    return fetch_products()
